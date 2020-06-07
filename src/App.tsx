a6de6842e03e109015b565326ee24dee4ea7a590
Viet Anh Le <mc.vietanh@gmail.com>
2020-06-07 13:24:04 +0700

    done v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TodoList } from "./TodoList/TodoList";
 
-function App() {
+import { TodoForm } from "./TodoList/TodoForm";
+
+type Todo = {
+  text: string;
+  complete: boolean;
+};
+
+const initialTodos: Array<Todo> = [
+  { text: " Walk the dog", complete: true },
+  { text: "Write an app", complete: false },
+];
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+  const toggleTodo: ToggleTodo = (selectedTodo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = (newTodo) => {
+    newTodo.trim() !== "" &&
+      setTodos([...todos, { text: newTodo, complete: false }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/TodoList/TodoListItem.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface TodoListItemProps {
+  todo: Todo;
+  toggleTodo: ToggleTodo;
+}
+
+export const TodoListItem: React.FC<TodoListItemProps> = ({
+  todo,
+  toggleTodo,
+}) => {
+  return (
+    <li>
+      <label
+        style={{ textDecoration: todo.complete ? "line-through" : "none" }}
+      >
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={() => toggleTodo(todo)}
+        />{" "}
+        {todo.text}
+      </label>
+    </li>
+  );
+};
